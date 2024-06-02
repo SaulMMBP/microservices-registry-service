8fb32b7a64ea5bdfbc65d916763b55162b6b553f
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2024-06-02 15:26:04 -0600

    unit tests

--- src/test/java/io/github/saulmmbp/registryService/MicroservicesRegistryServiceApplicationTests.java
+++ src/test/java/io/github/saulmmbp/registryService/MicroservicesRegistryServiceApplicationTests.java
@@ -1,13 +1,34 @@
 package io.github.saulmmbp.registryService;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Map;
 
-@SpringBootTest
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 class MicroservicesRegistryServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void catalogLoads() {
+	@SuppressWarnings("rawtypes")
+	ResponseEntity<Map> entity = testRestTemplate.withBasicAuth("admin", "admin").getForEntity("/eureka/apps", Map.class);
+
+	Assertions.assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void adminLoads() {
+	@SuppressWarnings("rawtypes")
+	ResponseEntity<Map> entity = testRestTemplate.withBasicAuth("admin", "admin").getForEntity("/actuator/env", Map.class);
+	Assertions.assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 
 }
